fef16f5a6adcf320d5d88e3df7b23948fc10e3f2
soheekim <soheekim.dev@gmail.com>
2021-06-02 17:37:01 +0900

    음..? rafce 잘못 썼었나 보다. 함수형 컴포넌트로 back함.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,11 @@
-import React from 'react'
 import Header from './components/Header'
 
-// function App() {
-//   return (
-//     <div className="container">
-//       <Header />
-//     </div>
-//   );
-// }
-
-class App extends React.Component {
-  render() {
-    return <h1>Hello from a class</h1>
-  }
+const App = () => {
+  return (
+    <div className='container'>
+      <Header />
+    </div>
+  )
 }
 
-export default App;
+export default App
