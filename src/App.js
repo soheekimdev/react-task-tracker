7a39948022c9a9f8e1be6d77c4a26e37d8f5c9e5
soheekim <soheekim.dev@gmail.com>
2021-06-02 16:28:11 +0900

    (rafce 플러그인 사용해서 만든) 함수형 컴포넌트가 아닌, 클래스형 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import React from 'react'
 import Header from './components/Header'
 
-function App() {
-  return (
-    <div className="container">
-      <Header />
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="container">
+//       <Header />
+//     </div>
+//   );
+// }
+
+class App extends React.Component {
+  render() {
+    return <h1>Hello from a class</h1>
+  }
 }
 
 export default App;
