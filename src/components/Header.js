ab94e6f3207243b553b2b4b73222957ff7b89980
soheekim <soheekim.dev@gmail.com>
2021-06-03 16:20:48 +0900

    스타일링: index.css 수정, Button.js 컴포넌트 만들어서 import

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 const Header = ({ title }) => {
   return (
-    <header>
-      <h1 style={headingStyle}>{title}</h1>
+    <header className='header'>
+      <h1>{title}</h1>
+      <Button color='green' text='Add' />
+      <Button color='blue' text='Hello' />
+      <Button text='Bye' />
     </header>
   )
 }
@@ -16,9 +20,10 @@ Header.propTypes = {
   title: PropTypes.string.isRequired,
 }
 
-const headingStyle = {
-  color: 'red',
-  backgroundColor: 'black'
-}
+// CSS in JS
+// const headingStyle = {
+//   color: 'red',
+//   backgroundColor: 'black'
+// }
 
 export default Header
