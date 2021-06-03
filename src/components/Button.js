a277db121cb26329fa249b7906e32463ce622ff0
soheekim <soheekim.dev@gmail.com>
2021-06-03 17:04:21 +0900

    버튼 onClick 이벤트 추가

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,16 +1,25 @@
 import PropTypes from 'prop-types'
 
-const Button = ({ color, text }) => {
-    return <button style={{ backgroundColor: color }} className='btn'>{text}</button>
+const Button = ({ color, text, onClick }) => {   
+  return (
+    <button
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      className='btn'
+    >
+      {text}
+    </button>
+  )
 }
 
 Button.defaultProps = {
-    color: 'black',
+  color: 'black',
 }
 
 Button.propTypes = {
-    text: PropTypes.string,
-    color: PropTypes.string,
+  text: PropTypes.string,
+  color: PropTypes.string,
+  onClick: PropTypes.func,
 }
 
 export default Button
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 const Header = ({ title }) => {
+  const onClick = () => {
+    console.log('click')
+  }
+
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button color='green' text='Add' />
-      <Button color='blue' text='Hello' />
-      <Button text='Bye' />
+      <Button color='green' text='Add' onClick={onClick} />
     </header>
   )
 }
