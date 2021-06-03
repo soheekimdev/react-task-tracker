8688d957bc3567880116edb0e04ba4adc4325dc1
soheekim <soheekim.dev@gmail.com>
2021-06-03 14:58:55 +0900

    스타일링: CSS in JS

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,17 +3,22 @@ import PropTypes from 'prop-types'
 const Header = ({ title }) => {
   return (
     <header>
-      <h1>{title}</h1>
+      <h1 style={headingStyle}>{title}</h1>
     </header>
   )
 }
 
-// Header.defaultProps = {
-//   title: 'Task Tracker',
-// }
+Header.defaultProps = {
+  title: 'Task Tracker',
+}
 
 Header.propTypes = {
   title: PropTypes.string.isRequired,
+}
+
+const headingStyle = {
+  color: 'red',
+  backgroundColor: 'black'
 }
 
 export default Header
