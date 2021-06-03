faafe417bb8677475bac6d06a14f940c908c8bf7
soheekim <soheekim.dev@gmail.com>
2021-06-03 11:36:58 +0900

    컴포넌트에 props 보내고 출력, defaultProps 프로퍼티 사용해서 초깃값 정의

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,13 @@
-const Header = () => {
+const Header = (props) => {
   return (
     <header>
-      <h1>Task Tracker</h1>
+      <h1>{props.title}</h1>
     </header>
   )
+}
+
+Header.defaultProps = {
+  title: 'Task Tracker',
 }
 
 export default Header
