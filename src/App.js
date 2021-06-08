df5e07a7f6982885f66d2d59dceed81ba61bbd50
soheekim <soheekim.dev@gmail.com>
2021-06-08 17:56:37 +0900

    Add task submit: Save Task 버튼 클릭하면 콘솔에 task 정보 표시

--- src/App.js
+++ src/App.js
@@ -25,6 +25,12 @@ const App = () => {
     }
   ])
 
+  // Add Task
+  const addTask = (task) => {
+    console.log(task)
+  }
+
+
   // Delete Task
   const deleteTask = (id) => {
     setTasks(tasks.filter((task) => task.id !== id))
@@ -38,7 +44,7 @@ const App = () => {
   return (
     <div className='container'>
       <Header />
-      <AddTask />
+      <AddTask onAdd={addTask} />
       {tasks.length > 0 ? (<Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />) : ('No Tasks To Show')}
     </div>
   )
