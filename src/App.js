388404910863c8769737d53ad4c3a0189e48b192
soheekim <soheekim.dev@gmail.com>
2021-06-08 18:35:01 +0900

    Add task submit: Save Task 버튼 클릭 시 tasks에 task 추가

--- src/App.js
+++ src/App.js
@@ -28,8 +28,8 @@ const App = () => {
   // Add Task
   const addTask = (task) => {
     const id = Math.floor(Math.random() * 10000) + 1
-
-    console.log(id)
+    const newTask = { id, ...task }
+    setTasks([...tasks, newTask])
   }
 
 
